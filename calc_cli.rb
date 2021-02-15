6c46ff48c0a1a7b424af2950a4f15006f7727726
rayalva407 <rayalva407@gmail.com>
2021-02-15 15:19:49 -0700

    creates multiply method

--- calc_cli.rb
+++ calc_cli.rb
@@ -25,7 +25,7 @@ while operation != "q"
     num1 = gets.chomp.to_i
     puts "Enter second number"
     num2 = gets.chomp.to_i
-    puts "#{num1} * #{num2} = #{num1 * num2}"
+    puts "The answer is #{calculator.multiply(num1, num2)}"
   elsif operation == '/'
     puts "Enter first number"
     num1 = gets.chomp.to_i
--- calculator.rb
+++ calculator.rb
@@ -7,4 +7,8 @@ class Calculator
   def subtract(x, y)
     x - y
   end
+
+  def multiply(x, y)
+    x * y
+  end
 end

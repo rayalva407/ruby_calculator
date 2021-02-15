d4c4c264e1bf4aadb6347c568d988d0a3a81f780
rayalva407 <rayalva407@gmail.com>
2021-02-15 15:20:36 -0700

    adds divide method

--- calc_cli.rb
+++ calc_cli.rb
@@ -31,7 +31,7 @@ while operation != "q"
     num1 = gets.chomp.to_i
     puts "Enter second number"
     num2 = gets.chomp.to_i
-    puts "#{num1} / #{num2} = #{num1 / num2}"
+    puts "The answer is #{calculator.divide(num1, num2)}"
   end
 end
 puts "Goodbye!"
--- calculator.rb
+++ calculator.rb
@@ -11,4 +11,8 @@ class Calculator
   def multiply(x, y)
     x * y
   end
+
+  def divide(x, y)
+    x / y
+  end
 end

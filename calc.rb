ec448fcff4dc361c7e52a5e0629b4a12c2964df4
rayalva407 <rayalva407@gmail.com>
2021-02-14 20:41:06 -0700

    first commit

---
+++ calc.rb
@@ -0,0 +1,33 @@
+operation = ""
+puts "Welcome to Ruby Calculator"
+while operation != "q"
+  puts "Choose operation. Type '+' to add, '-' to subtract, '*' to multiply, or '/' to divide"
+  puts "Type 'q' to quit"
+  operation = gets.chomp
+  if operation == '+'
+    puts "Enter first number"
+    num1 = gets.chomp.to_i
+    puts "Enter second number"
+    num2 = gets.chomp.to_i
+    puts "#{num1} + #{num2} = #{num1 + num2}"
+  elsif operation == '-'
+    puts "Enter first number"
+    num1 = gets.chomp.to_i
+    puts "Enter second number"
+    num2 = gets.chomp.to_i
+    puts "#{num1} - #{num2} = #{num1 - num2}"
+  elsif operation == '*'
+    puts "Enter first number"
+    num1 = gets.chomp.to_i
+    puts "Enter second number"
+    num2 = gets.chomp.to_i
+    puts "#{num1} * #{num2} = #{num1 * num2}"
+  elsif operation == '/'
+    puts "Enter first number"
+    num1 = gets.chomp.to_i
+    puts "Enter second number"
+    num2 = gets.chomp.to_i
+    puts "#{num1} / #{num2} = #{num1 / num2}"
+  end
+  puts "Goodbye!"
+end
